497154035b25ed919d2ac4af2e68e74c8ddf61b2
codemox <viranimoxil@gmail.com>
2026-01-22 21:59:21 +0530

    datatypes

---
+++ js-lerning/js_02.js
@@ -0,0 +1,21 @@
+//alert("hello") we are using nodejs not browser
+
+let name = "moxil"
+let age = 18
+let isLoggedIn = false
+
+
+// number => 2 to power 53
+//BigInt
+//String => ""
+//Boolean => true/false
+//null=> standalone value
+//undefined =>
+//symbol => fot unique talk
+
+
+//object
+
+//for knowing the type of any var...
+console.log(typeof "undefined"); // undefined
+console.log(typeof "null"); // null
